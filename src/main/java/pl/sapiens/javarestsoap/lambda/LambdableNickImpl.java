c925dea23d4c25542ff6f96677386a73fa96d3a7
Adam Kozuch <akozuch87@gmail.com>
2022-06-25 09:30:26 +0200

    [change] -modified the lambda pack

--- src/main/java/pl/sapiens/javarestsoap/lambda/LambdableNickImpl.java
+++ src/main/java/pl/sapiens/javarestsoap/lambda/LambdableNickImpl.java
@@ -1,9 +1,11 @@
 package pl.sapiens.javarestsoap.lambda;
 
-public class LambdableNick implements Lambdable{
+public class LambdableNickImpl implements Lambdable {
+
 
     @Override
     public String getNick() {
         return "Adam";
+
     }
 }
--- src/main/java/pl/sapiens/javarestsoap/lambda/Main.java
+++ src/main/java/pl/sapiens/javarestsoap/lambda/Main.java
@@ -1,26 +1,48 @@
 package pl.sapiens.javarestsoap.lambda;
 
+import javax.enterprise.inject.spi.Producer;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getNick() != null ? getNick().length(): 0);
+        System.out.println(getNick() != null ? getNick().length() : 0);
 
         maybeNick()
                 .map(nick -> nick.length())
-                .ifPresent(integer -> System.out.println("Nick length: " + integer));
+                .ifPresent(integer -> System.out.println("Nick len: " + integer));
 
+        // very old java - always
+        Lambdable oldWay = new LambdableNickImpl();
+
+        // we don't know class name - till 1.7
+        Lambdable anonymousWay = new Lambdable() {
+            @Override
+            public String getNick() {
+                return "maniek";
+            }
+        };
+
+        Lambdable anonymousWay2 = new Lambdable() {
+            @Override
+            public String getNick() {
+                return "maniek";
+            }
+        };
+
+        Lambdable newWay = () -> "maniek";
+        Supplier<String> standardSupplier = () -> "maniek";
+        Adult checker = (age) -> {
+            System.out.println("checking......");
+            return age > 18;
+        };
     }
 
-
-
-    static Optional<String> maybeNick () {
+    static Optional<String> maybeNick() {
         return Optional.ofNullable(null);
     }
-
     static String getNick() {
         return null;
     }
-
 }
